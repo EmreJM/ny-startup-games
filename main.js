588c3dc7da0f926b381ad8df269210ba09cd5ea5
Tatu Arvela <tatu.arvela@gmail.com>
2017-02-12 20:38:17 +0200

    implement basic UI elements

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-$.mobile.defaultPageTransition = "slideup";
+$.mobile.defaultPageTransition = "flow";
+
+$( document ).ready(function() {
+	$( "#pauseMenu, #credits" ).popup();
+})
+
+$( document ).on( "pagecontainershow", $.mobile.pageContainer, function ( e, ui ) {
+	if ($.mobile.activePage.attr('id') == "mainmenu") {
+		$("#menubutton").hide();
+	}
+	else {
+		$("#menubutton").show();
+	}
+});
